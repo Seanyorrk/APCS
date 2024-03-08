9d3218d49e75085f24c9c872b45f7de40880e40e
Seanyorrk <2024650@nfaschool.org>
2024-03-08 08:19:02 -0500

    Updates

---
+++ Sorts/Example.java
@@ -0,0 +1,19 @@
+package Sorts;
+
+public class Example {
+    public static void main(String[] args) {
+        int items[] = {5, 3, 7, 6, 2, 8, 1, 9, 4, 0};
+
+        Sorts.selectionSort(items);
+
+        for (int i = 0; i < items.length; i++) {
+            System.out.print(items[i] + " ");
+        }
+
+        Sorts.insertionSort(items);
+
+        for (int i = 0; i < items.length; i++) {
+            System.out.print(items[i] + " ");
+        }
+    }
+}
--- Sorts/Sorts.java
+++ Sorts/Sorts.java
@@ -62,9 +62,38 @@ public class Sorts {
         }
     }
 
-    public static void ArrayListSort(ArrayList<Comparable> items) {
+    // Sorts an array of integers using Merge Sort
+    private static void merge(int[] items, int start,
+            int mid, int end) {
+        int[] temp = new int[items.length];
+        int pos1 = start;
+        int pos2 = mid + 1;
+        int spot = start;
 
-        
+        while (!(pos1 > mid && pos2 > end)) {
+            if ((pos1 > mid) || ((pos2 <= end) && (items[pos2] < items[pos1]))) {
+                temp[spot] = items[pos2];
+                pos2 += 1;
+            } else {
+                temp[spot] = items[pos1];
+                pos1 += 1;
+            }
+            spot += 1;
+        }
+        /* copy values from temp back to items */
+        for (int i = start; i <= end; i++) {
+            items[i] = temp[i];
+        }
+    }
+
+    // Sorts an array of integers using Merge Sort
+    public static void mergesort(int[] items, int start, int end) {
+        if (start < end) {
+            int mid = (start + end) / 2;
+            mergesort(items, start, mid);
+            mergesort(items, mid + 1, end);
+            merge(items, start, mid, end);
+        }
     }
 
 }
